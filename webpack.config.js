b5a1155de76c5d9929b1a3fb4af9bdda84d1eea1
Yoshimasa Suzuki <cognac0004@gmail.com>
2022-04-22 10:35:46 +0900

    typescriptの設定
    edit webpack.config.js and run "yarn run tsc --init"

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,21 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.tsx',
   output: {
       path: path.join(__dirname, 'public/js'),
       filename: 'bundle.js'
   },
+  resolve: {
+    extensions: ['.js', '.json', '.ts', '.tsx'],
+  },
   devtool: 'inline-source-map',
   module: {
     rules: [
-      { test: /\.tsx$/, loader: 'ts-loader' }
+      { test: /\.tsx?$/, loader: 'ts-loader' }
     ]
   },
+  target: ["web", "es5"],
   devServer: {
     static: {
       directory: path.join(__dirname, 'public'),
